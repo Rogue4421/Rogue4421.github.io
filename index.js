6d6141172196db822bf6614f60b3d206116b5b24
Rogue4421 <98064283+Rogue4421@users.noreply.github.com>
2022-02-17 15:16:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var x = "12345"
+
+function passcheck(document.getElementById("verify").innerhtml) {
+if(document.getElementById("password").value != x) {
+alert("Invalid Credentials");
+return false;
+}
+}
